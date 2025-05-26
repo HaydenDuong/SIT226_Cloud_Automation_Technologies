f3b98c5dea00cb290bea9ae08a03aa277d8a62a9
HaydenDuong <tamlac20121996@gmail.com>
2025-05-26 16:14:50 +1000

    pre - 1/6

--- Coding Tasks/HealthcareBooking/app.py
+++ Coding Tasks/HealthcareBooking/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,7 +7,7 @@ def home():
 
 @app.route('/book')
 def book():
-    return "Book an Appointment"
+    return render_template('book.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 5000)
--- Coding Tasks/HealthcareBooking/db_setup.sql
+++ Coding Tasks/HealthcareBooking/db_setup.sql
@@ -6,5 +6,5 @@ CREATE TABLE appointments (
     id SERIAL PRIMARY KEY,
     patient_name VARCHAR(100),
     doctor_id INT,
-    appointment_time TIMESTAMP,
+    appointment_time TIMESTAMP
 );
