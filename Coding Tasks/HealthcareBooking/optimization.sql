99cc2b4f7190b1a70e0398209d7aa8db24e590fc
HaydenDuong <tamlac20121996@gmail.com>
2025-05-30 22:07:36 +1000

    Commit HD

--- Coding Tasks/HealthcareBooking/optimization.sql
+++ Coding Tasks/HealthcareBooking/optimization.sql
@@ -1,1 +1,3 @@
+CREATE INDEX idx_patient_name ON appointments (patient_name);
+CREATE INDEX idx_doctor_id ON appointments (doctor_id);
 CREATE INDEX idx_appointment_time ON appointments (appointment_time); 
